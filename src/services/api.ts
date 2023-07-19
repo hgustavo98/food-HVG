d14597a38bbd7ee43eba4535f0becbcb6f18e774
Gustavo Lima <79370990+hgustavo98@users.noreply.github.com>
2023-07-19 13:39:05 -0300

    Status atualizado para exbir requisições

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,20 +1,46 @@
-import axios from 'axios'
-import { CustomerData } from '../interfaces/CustomerData'
-import { Snack } from '../interfaces/Snack'
-import { SnackData } from '../interfaces/SnackData'
+import axios from 'axios';
+import { CustomerData } from '../interfaces/CustomerData';
+import { Snack } from '../interfaces/Snack';
+import { SnackData } from '../interfaces/SnackData';
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-})
+});
 
+export const getBurgers = (filter: string | null) =>
+  api.get<SnackData[]>(`/snacks/${filter}?snack=burger`);
 
+export const getPizzas = (filter: string | null) =>
+  api.get<SnackData[]>(`/snacks/${filter}?snack=pizza`);
 
-export const getBurgers = (filter: string | null) => api.get<SnackData[]>(`/snacks/${filter}?snack=burger`);
-export const getPizzas = (filter: string | null) => api.get<SnackData[]>(`/snacks/${filter}?snack=pizza`);
-export const getDrinks = (filter: string | null) => api.get<SnackData[]>(`/snacks/${filter}?snack=drink`);
-export const getIceCreams = (filter: string | null) => api.get<SnackData[]>(`/snacks/${filter}?snack=ice-cream`);
+export const getDrinks = (filter: string | null) =>
+  api.get<SnackData[]>(`/snacks/${filter}?snack=drink`);
 
+export const getIceCreams = (filter: string | null) =>
+  api.get<SnackData[]>(`/snacks/${filter}?snack=ice-cream`);
 
+// Novas requisições:
 
+// Rota para obter os nomes dos lanches (snackName) e os nomes dos clientes (customerName) para os itens de pedidos pagos
+export const getPaidOrders = () => api.get<unknown>('/paid-orders');
+
+// Rota para obter todos os pedidos de um cliente específico (orderId, status, total, snackName, snackPrice).
+export const getCustomerOrders = (customerId: number) => api.get<unknown>(`/customer-orders/${customerId}`);
+
+// Rota para obter os itens de pedido por orderId
+export const getOrderItems = (orderId: number) => api.get<unknown>(`/order-items/${orderId}`);
+
+// Rota para obter os pedidos com itens com quantidade maior que 2 e inclui informações do cliente e dos lanches (customerName, totalSpent, snackName, snackPrice).
+export const getOrdersWithItemsQuantityGreaterThan2 = () => api.get<unknown>('/orders-with-items-quantity-greater-than-2');
+
+// Rota para obter o valor total gasto por cada cliente e inclui informações sobre os itens de pedidos e os lanches.
+export const getCustomersTotalSpent = () => api.get<unknown>('/customers-total-spent');
+
+// Rota para obter o valor total gasto por um cliente específico (customerTotalSpent).
+export const getCustomerTotalSpent = (customerId: number) => api.get<unknown>(`/customer-total-spent/${customerId}`);
+
+// Rota para obter o número total de itens em pedidos de um cliente (customerTotalItems).
+export const getCustomerTotalItems = (customerId: number) => api.get<unknown>(`/customer-total-items/${customerId}`);
 
 export const processCheckout = (cart: Snack[], customer: CustomerData) =>
   api.post('/checkout', {
@@ -40,6 +66,6 @@ export const processCheckout = (cart: Snack[], customer: CustomerData) =>
       ).getFullYear()}`,
       creditCardSecurityCode: customer.creditCardSecurityCode,
     },
-  })
+  });
 
-export default api
+export default api;
